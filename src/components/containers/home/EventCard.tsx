e44e30163d9e77ccd029036829d5b2303149c546
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-10 15:22:40 -0300

    Added swiperjs

--- src/components/containers/home/EventCard.tsx
+++ src/components/containers/home/EventCard.tsx
@@ -1,7 +1,6 @@
-"use client";
-
 import Image from 'next/image';
-import React, { useState } from 'react';
+import React from 'react';
+import ImagesSwiper from './ImagesSwiper';
 
 const EventCard: React.FC<IEventCard> = ({
   artist,
@@ -13,55 +12,11 @@ const EventCard: React.FC<IEventCard> = ({
   images,
   hasPaymentOptions = false
 }) => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-  const nextImage = () => {
-    setCurrentImageIndex((prev) => (prev + 1) % images.length);
-  };
-
-  const prevImage = () => {
-    setCurrentImageIndex((prev) => (prev - 1 + images.length) % images.length);
-  };
   return (
     <div className="bg-cards-container rounded-xs overflow-hidden shadow-2xl w-xl h-fit mx-auto">
-      <div className="relative h-[36rem]">
-        <img 
-          src={images[currentImageIndex]} 
-          alt={`${artist} - ${currentImageIndex + 1}`}
-          className="w-full aspect-square h-full object-cover"
-        />
-
-         {images.length > 1 && (
-          <>
-            <button
-              onClick={prevImage}
-              className="absolute cursor-pointer left-3 sm:left-6 top-1/2 transform -translate-y-1/2 bg-primary-white hover:opacity-70 rounded-xl p-3 transition-opacity"
-            >
-              <Image src="/icons/arrow.svg" width={24} height={24} alt="arrow icon" />
-            </button>
-            <button
-              onClick={nextImage}
-              className="absolute cursor-pointer right-3 sm:right-6 top-1/2 transform -translate-y-1/2 bg-primary-white hover:opacity-70 rounded-xl p-3 transition-opacity rotate-180"
-            >
-              <Image src="/icons/arrow.svg" width={24} height={24} alt="arrow icon" />
-            </button>
-          </>
-        )}
-        {images.length > 1 && (
-            <div className="absolute bottom-4 w-full justify-center flex space-x-1.5">
-              {images.map((_, index) => (
-                <button
-                  key={index}
-                  onClick={() => setCurrentImageIndex(index)}
-                  className={`w-[7px] h-[7px] rounded-full transition-colors ${
-                    index === currentImageIndex ? 'bg-white' : 'bg-red-200/40'
-                  }`}
-                />
-              ))}
-            </div>
-          )}
-      </div>
-
+      
+        <ImagesSwiper  images={images} />
+      
 
       {/* Event Details */}
       <div className="p-3.5 pt-3">
---
+++ src/components/containers/home/ImagesSwiper.tsx
@@ -0,0 +1,61 @@
+"use client"
+
+import { useRef } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import Image from 'next/image';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+export default function CustomSwiper({ images }: { images: IEventCard['images'] }) {
+  const prevRef = useRef(null);
+  const nextRef = useRef(null);
+
+  return (
+    <div className="relative w-full">
+      {/* Botones custom */}
+      <button ref={prevRef} className="absolute z-10 top-1/2 left-3 sm:left-6 -translate-y-1/2 bg-primary-white hover:opacity-75 active:scale-95 p-3 rounded-xl shadow transition-all">
+        <Image src="/icons/arrow.svg" width={24} height={24} alt="arrow icon" />
+      </button>
+      <button ref={nextRef} className="absolute z-10 top-1/2 right-3 sm:right-6 -translate-y-1/2 bg-primary-white hover:opacity-75 active:scale-95 p-3 rounded-xl shadow transition-all rotate-180">
+        <Image src="/icons/arrow.svg" width={24} height={24} alt="arrow icon" />
+      </button>
+
+      <Swiper
+        loop={true}
+        modules={[Navigation, Pagination]}
+        navigation={{
+          prevEl: prevRef.current,
+          nextEl: nextRef.current,
+        }}
+        pagination={{ clickable: true }}
+        onBeforeInit={(swiper) => {
+          if (
+            swiper &&
+            typeof swiper.params.navigation === 'object' &&
+            swiper.params.navigation !== null
+          ) {
+            swiper.params.navigation.prevEl = prevRef.current;
+            swiper.params.navigation.nextEl = nextRef.current;
+          }
+        }}
+        spaceBetween={20}
+        slidesPerView={1}
+      >
+        {images.map((image, index) => (
+          <SwiperSlide key={image}>
+            <Image
+              width={1000}
+              height={1000}
+              src={image}
+              alt={`event ${index + 1}`}
+              className="w-full aspect-square object-cover h-[36rem]"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+}
