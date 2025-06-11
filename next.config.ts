533bd924ad0cc7e3cc5528df84faa6bce372a5e0
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-11 10:02:40 -0300

    Changed svg to svgr

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+
   images: {
     domains: ["images.unsplash.com", "plus.unsplash.com"],
   },
-  /* config options here */
 };
 
 export default nextConfig;
