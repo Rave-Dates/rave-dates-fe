863dba67b712d4e1bb17b784b8afe1125b28977c
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-12 14:20:25 -0300

    Improved filter modal

--- src/components/containers/navbar/FilterModal.tsx
+++ src/components/containers/navbar/FilterModal.tsx
@@ -87,8 +87,8 @@ function FilterModal() {
         <FilterSvg />
       </button>
       {isModalOpen && (
-        <div onClick={() => setIsModalOpen(false)} className="fixed inset-0 bg-black/60 bg-opacity-75 flex items-center justify-center md:p-4 z-50">
-          <div onClick={(e) => e.stopPropagation()}  className="bg-[#050505] flex flex-col justify-between rounded-2xl w-full md:h-fit max-h-full md:max-w-md overflow-y-scroll shadow-2xl">
+        <div onClick={() => setIsModalOpen(false)} className="fixed inset-0 bg-black/60 bg-opacity-75 flex items-center justify-center md:py-8 z-50">
+          <div onClick={(e) => e.stopPropagation()}  className="bg-[#050505] flex flex-col rounded-2xl w-full md:h-fit max-h-full md:max-w-md overflow-hidden shadow-2xl">
             {/* Header */}
             <div className="flex flex-row-reverse md:flex-row justify-end gap-5 items-center md:justify-between pt-8 px-6"> 
               <h2 className="text-title">Filtros</h2>
@@ -100,7 +100,7 @@ function FilterModal() {
               </button>
             </div>
 
-            <div className="py-6 space-y-8 font-light px-6">
+            <div className="py-6 space-y-8 overflow-y-scroll font-light px-6">
               {/* Location Filter */}
               <CheckInput
                 items={locations}
