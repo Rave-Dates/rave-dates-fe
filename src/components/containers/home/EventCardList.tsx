7f19274c29f54775dfa919b0f372920c8861686b
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-12 14:25:25 -0300

    Home padding fix

--- src/components/containers/home/EventCardList.tsx
+++ src/components/containers/home/EventCardList.tsx
@@ -5,7 +5,7 @@ import { events } from '@/template-data';
 const EventCardList: React.FC = () => {
 
   return (
-    <div className="py-8 sm:pt-[7.5rem] bg-primary-black mx-auto px-6">
+    <div className="py-8 pb-32 sm:pb-8 sm:pt-[7.5rem] bg-primary-black mx-auto px-6">
       <div className="space-y-4">
         {events.map((event) => (
           <div key={event.id} className="flex justify-center">
