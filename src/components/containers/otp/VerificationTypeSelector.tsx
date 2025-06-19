168743f0c6e6b292fadbebcb526a7cc1e2880865
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-06-19 11:53:54 -0300

    Added otp view

---
+++ src/components/containers/otp/VerificationTypeSelector.tsx
@@ -0,0 +1,40 @@
+type Props = {
+  selected: string;
+  setSelected: (value: string) => void;
+};
+
+export default function VerificationTypeSelector({ selected, setSelected }: Props) {
+  const methods = ["Email", "WhatsApp"];
+
+  return (
+    <div className="bg-cards-container rounded-lg">
+      {methods.map((item) => (
+        <label
+          key={item}
+          className="flex items-center w-full justify-between cursor-pointer group py-4 not-last:border-b border-inactive"
+        >
+          <div className="order-last relative pe-4">
+            <input
+              type="radio"
+              name="paymentType"
+              value={item}
+              checked={selected === item}
+              onChange={() => setSelected(item)}
+              className="sr-only"
+            />
+            <div
+              className={`w-6 h-6 rounded-full border-1 flex items-center justify-center transition-colors ${
+                selected === item
+                  ? "border-inactive bg-primary-black"
+                  : "border-inactive group-hover:border-primary/30"
+              }`}
+            >
+              {selected === item && <div className="w-3.5 h-3.5 bg-primary rounded-full" />}
+            </div>
+          </div>
+          <span className="group-hover:text-lime-200 font-light ps-4 transition-colors">{item}</span>
+        </label>
+      ))}
+    </div>
+  );
+}
