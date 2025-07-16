71b9870652dab31558e84921987c8e3cf1d8d902
Nazareno Gutierrez <nazarenojunin@gmail.com>
2025-07-16 19:25:13 -0300

    env changes

--- src/utils/envHelper.ts
+++ src/utils/envHelper.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.NODE_ENV === "development" ? process.env.NEXT_PUBLIC_BASE_URL_DEV : process.env.NEXT_PUBLIC_BASE_URL;
+export const BASE_URL = process.env.ENVIRONMENT === "dev" ? process.env.NEXT_PUBLIC_BASE_URL_DEV: process.env.NEXT_PUBLIC_BASE_URL ;
